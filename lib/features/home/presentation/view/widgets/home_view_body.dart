141455621a27d6fac5a05806a93229369eab6433
mohamed naif <naifm371@gmail.com>
2023-12-16 09:37:58 +0200

    save some changes

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../const.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -7,10 +9,13 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: kLightColor,
         centerTitle: true,
-        elevation: 1,
+        elevation: 0,
         title: SizedBox(
-            height: 80, width: 120, child: Image.asset('assets/حكواتي-02.png')),
+            height: 80,
+            width: 120,
+            child: Image.asset('assets/images/حكواتي-02.png')),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -20,8 +25,8 @@ class HomeViewBody extends StatelessWidget {
               decoration: const BoxDecoration(
                   shape: BoxShape.circle,
                   image: DecorationImage(
-                      image:
-                          AssetImage('assets/images/photo_2023-12-14_16-40-34.jpg'))),
+                      image: AssetImage(
+                          'assets/images/photo_2023-12-14_16-40-34.jpg'))),
 
               // backgroundColor: Colors.black,
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hakawaty/const.dart';
 import 'package:hakawaty/features/auth/presentation/view/register_view.dart';
+import 'package:hakawaty/features/home/presentation/view/home_view.dart';
 
 // import 'features/home/presentation/view/home_view.dart';
 
@@ -19,9 +20,9 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Cairo',
-          scaffoldBackgroundColor: kDarkColor,
+          scaffoldBackgroundColor: kLightColor,
         ),
-        home: const RegisterView(),
+        home: const HomeView(),
       ),
     );
   }
