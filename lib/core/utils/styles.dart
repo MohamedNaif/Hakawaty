659d1be8e21877f4d6b0f4e89fc1445034a22bc3
احمد ناصر <eng.ahmednasser2024@gmail.com>
2023-12-23 13:58:10 +0200

    some refactor on home view and creat style utils

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hakawaty/const.dart';
+
+abstract class Style {
+  static const TextStyle textStyle22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    color: kDarkColor,
+  );
+}
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../../const.dart';
-// import '../../../../category/presentation/views/category_view.dart';
-import '../../../../profile/presentation/view/profile.dart';
+import '../../../../../core/utils/styles.dart';
 import '../../../data/category_list_data.dart';
-// import 'category_card.dart';
 import '../../../data/top_story_list_data.dart';
-// import 'top_score_card.dart';
+import 'profile_image.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -24,58 +22,18 @@ class HomeViewBody extends StatelessWidget {
           width: 120,
           child: Image.asset('assets/images/حكواتي-03.png'),
         ),
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ProfileScreen(),
-                ),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Container(
-                // height: 70,
-                width: 40,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/photo_2023-12-14_16-40-34.jpg',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+        actions: const [ProfileImage()],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Categories',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-                color: kDarkColor,
-              ),
-            ),
+            const Text('Categories', style: Style.textStyle22),
             const Gap(10),
             CategoryList(),
             const Gap(20),
-            const Text(
-              'Top Story',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-                color: kDarkColor,
-              ),
-            ),
+            const Text('Top Story', style: Style.textStyle22),
             const Gap(10),
             Expanded(child: TopStoryList()),
           ],
@@ -84,5 +42,3 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
-
-
